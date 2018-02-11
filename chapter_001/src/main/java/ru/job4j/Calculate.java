3541610d8556348984ce2790ec5d6d99eab7f15a
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-11 13:35:23 +0300

    1.3. JavDoc. Code convetion.[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
+
+/**
+* Class Класс для вычисления арифметических действий
+* @author grusanov
+* @since 11.02.2018
+* @version 1
+*/
 public class Calculate{
 
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String args []){
 
 		System.out.println("Hello, world!");
