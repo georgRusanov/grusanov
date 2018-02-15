a2b8d0651e9f33eb48c7c618326080dea2511c45
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-15 12:41:09 +0300

    Create Matrix.java

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * @author Georg Rusanov (rusanovgeorgy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Matrix {
+
+    public int[][] multiple(int size){
+        int[][] array = new int[size][size];
+        for (int i = 0; i != size; i++) {
+            for (int j = 0; j != size; j++) {
+               array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+}
