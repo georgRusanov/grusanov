306f06878ba4279d935b53c26d0a0638da4322e2
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-15 15:06:25 +0300

    Create ArrayDuplicateTest.java

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,21 @@
+
+
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Georg Rusanov (rusanovgeorgy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
+    }
+}
