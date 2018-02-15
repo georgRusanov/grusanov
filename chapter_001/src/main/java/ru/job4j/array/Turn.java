b5a7d32d4da63f326204ae296e4349348b11bc77
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-15 22:08:09 +0300

    5.2. Перевернуть массив.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -7,9 +7,12 @@ package ru.job4j.array;
  */
 public class Turn {
 
-    public static int[] back(int[] array){
+    public int[] back(int[] array){
+        int temp;
         for (int index = 0; index != array.length / 2; index++) {
+            temp = array[index];
             array[index] = array[array.length - 1 - index];
+            array[array.length - 1 - index] = temp;
         }
         return array;
     }
--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -16,7 +16,8 @@ public class TurnTest {
     @Test
     public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
         int[] array = {4, 1, 6, 2};
-        int[] result = Turn.back(array);
+        Turn turn = new Turn();
+        int[] result = turn.back(array);
         int[] expected = {2, 6, 1, 4};
         assertThat(result, is(expected));
     }
@@ -24,7 +25,8 @@ public class TurnTest {
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
         int[] array = {1, 2, 3, 4, 5};
-        int[] result = Turn.back(array);
+        Turn turn = new Turn();
+        int[] result = turn.back(array);
         int[] expected = {5, 4, 3, 2, 1};
         assertThat(result, is(expected));
     }
