947f92fc620a109cdef16ab6c10a7a41a7ee1496
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-15 12:53:31 +0300

    Create MatrixTest.java

---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,32 @@
+
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Georg Rusanov (rusanovgeorgy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixTest {
+
+    @Test
+    public void WhenFourAndFourThanSixteen() {
+        Matrix matrix = new Matrix();
+        int[][] result = matrix.multiple(5);
+        int[] expected = 16;
+        assertThat(result[3][3], is(expected));
+    }
+    
+    @Test
+    public void WhenEightAndNinetThanSeventyTwo() {
+        Matrix matrix = new Matrix();
+        int[][] result = matrix.multiple(9);
+        int[] expected = 72;
+        assertThat(result[8][7], is(expected));
+    }
+}
