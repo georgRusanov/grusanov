cdd47b6001cacb815a545fd2f67b892d51e9fede
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-15 12:33:17 +0300

    Create BubbleSortTest.java

---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Georg Rusanov (rusanovgeorgy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BubbleSortTest {
+
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        int[] array = {5, 1, 2, 7, 3};
+        int result = BubbleSort.sort(array);
+        int[] expected = {1, 2, 3, 5, 7};
+        assertThat(result, is(expected));
+    }
+}
