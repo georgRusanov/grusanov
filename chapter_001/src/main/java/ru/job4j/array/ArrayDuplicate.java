64e2b503b2f981148cd57bf7da02c832fab6f868
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-15 15:05:05 +0300

    Create ArrayDuplicate.java

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * @author Georg Rusanov (rusanovgeorgy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayDuplicate {
+
+    public String[] remove(String[] array){
+        for (int index = 0; index != data.length; index++) {
+            -1;
+        }
+        return -1;
+    }
+}
