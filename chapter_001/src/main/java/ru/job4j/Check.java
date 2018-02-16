442e1c6c16eb8936603e05ff53e429fc0d289a92
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-16 08:27:34 +0300

    Создал второй модуль и начал выполнять проверочное задание

---
+++ chapter_001/src/main/java/ru/job4j/Check.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+/**
+ * @author Georg Rusanov  (rusanovgeorgy@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Check {
+
+
+    boolean contains(String origin, String sub) {
+        char[] word = origin.toCharArray();
+        char[] letters = sub.toCharArray();
+
+    }
+}
