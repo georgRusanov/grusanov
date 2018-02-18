47a82be5cf42137f39f94f80a7125590e760a932
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-18 21:33:31 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.

--- chapter_002/src/test/java/ru/job4j/tracker/StubUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StubUITest.java
@@ -1,5 +1,10 @@
 package ru.job4j.tracker;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -9,10 +14,22 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class StubUITest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+    Tracker tracker = new Tracker();
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
 
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-        Tracker tracker = new Tracker();
         Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[0].getName(), is("test name"));
@@ -20,7 +37,6 @@ public class StubUITest {
 
     @Test
     public void whenUpdateThenTrackerHasUpdatedValue() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name1", "desc1"));
         Input input = new StubInput(new String[]{"2", "test name", "desc", item.getId(), "6"});
         new StartUI(input, tracker).init();
@@ -29,12 +45,38 @@ public class StubUITest {
 
     @Test
     public void whenDeleteThenTrackerHasNextValue() {
-        Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc"));
         Item item1 = tracker.add(new Item("test name1", "desc1"));
         Item item2 = tracker.add(new Item("test name2", "desc2"));
         Input input = new StubInput(new String[]{"3", item1.getId(), "6"});
         new StartUI(input, tracker).init();
         assertThat(tracker.getAll()[1].getName(), is("test name2"));
+    }
+
+    @Test
+    public void whenShowAllThenTrackerPrintTwoNames() {
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item1 = tracker.add(new Item("test name1", "desc1"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(true, is(new String(out.toByteArray()).contains("Заявка: test name\nЗаявка: test name1")));
+    }
+
+    @Test
+    public void whenFindByIdThenTrackerPrintOneName() {
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item1 = tracker.add(new Item("test name1", "desc1"));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(true, is(new String(out.toByteArray()).contains("Заявка: " + item.getName())));
+    }
+
+    @Test
+    public void whenFindByNameThenTrackerPrintOneName() {
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item1 = tracker.add(new Item("test name1", "desc1"));
+        Input input = new StubInput(new String[]{"5", item.getName(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(true, is(new String(out.toByteArray()).contains("Заявка: " + item.getName())));
     }
 }
