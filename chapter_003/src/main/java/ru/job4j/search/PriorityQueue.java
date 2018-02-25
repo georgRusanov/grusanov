4e2e74ba3f51b2f2a025f202859ec07e5ef42ee7
georgRusanov <rusanovgeorgy@gmail.com>
2018-02-26 00:56:23 +0300

    2. Очередь с приоритетом на LinkedList

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,36 @@
+package ru.job4j.search;
+
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        if (tasks.size() == 0) {
+            this.tasks.add(0, task);
+        } else if (task.getPriority() == 1){
+            this.tasks.add(0, task);
+        } else {
+            for (int index = 0; index < tasks.size(); index++) {
+                if (this.tasks.get(index).getPriority() > task.getPriority()) {
+                    this.tasks.add(index, task);
+                    break;
+                }
+                if (index == tasks.size() - 1) {
+                    this.tasks.addLast(task);
+                }
+            }
+        }
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
