cd857f725527b129b2c839b83ef66d480cf9b8ca
georgRusanov <rusanovgeorgy@gmail.com>
2018-03-16 22:32:52 +0300

    5.1.4. Создать convert(Iterator<Iterator>)

--- chapter_003/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_003/src/main/java/ru/job4j/iterator/Converter.java
@@ -1,22 +1,38 @@
 package ru.job4j.iterator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Converter {
 
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        List<Iterator<Integer>> iterators = new ArrayList<>();
+        while (it.hasNext()) {
+            iterators.add(it.next());
+        }
         return new Iterator<Integer>() {
+            int index = 0;
             @Override
             public boolean hasNext() {
-                return false;
+                boolean answer = false;
+                for (; index < iterators.size(); index++) {
+                    if (iterators.get(index).hasNext()) {
+                        answer = true;
+                        break;
+                    }
+                }
+                return answer;
             }
 
             @Override
             public Integer next() {
-                return null;
+                if (!hasNext()) {
+                    throw new NoSuchElementException("Больше нет элементов");
+                }
+                return iterators.get(index).next();
             }
         };
     }
-
-
 }
