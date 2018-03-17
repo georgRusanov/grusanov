9075f34b71cec41ffc9a79e76325543ca548c570
georgRusanov <rusanovgeorgy@gmail.com>
2018-03-17 11:22:20 +0300

    5.2.1. Реализовать SimpleArray<T>

---
+++ chapter_003/src/main/java/ru/job4j/Generic/SimpleArray.java
@@ -0,0 +1,54 @@
+package ru.job4j.Generic;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable<T> {
+    Object[] objects;
+    int index = 0, position = 0;
+
+    public SimpleArray() {
+        this.objects = new Object[10];
+    }
+
+    public SimpleArray(int size) {
+        this.objects = new Object[size];
+    }
+
+    public  void add(T model) {
+        if(index == objects.length) {
+            System.arraycopy(objects, 0, objects, 0, objects.length * 3 / 2 + 1);
+        }
+        this.objects[this.index++] = model;
+    }
+
+    public void set(int index, T model) {
+        this.objects[index] = model;
+    }
+
+    public void delete(int index) {
+        System.arraycopy(objects, index + 1, objects, index, objects.length - index - 1);
+    }
+
+    public T get(int index) {
+        return (T) this.objects[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return position <= index;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("Больше нет элементов");
+                }
+                return (T) objects[position++];
+            }
+        };
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/Generic/SimpleArrayTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.Generic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleArrayTest {
+    private SimpleArray<Integer> array = new SimpleArray<Integer>();
+
+    @Before
+    public void addElement() {
+        array.add(1);
+        array.add(2);
+    }
+
+    @Test
+    public void addTest() {
+        assertThat(array.iterator().next(), is(1));
+        assertThat(array.iterator().next(), is(2));
+    }
+
+    @Test
+    public void setTest() {
+        array.set(1,3);
+        assertThat(array.iterator().next(), is(1));
+        assertThat(array.iterator().next(), is(3));
+    }
+
+    @Test
+    public void deleteTest() {
+        array.delete(0);
+        assertThat(array.iterator().next(), is(2));
+    }
+
+    @Test
+    public void get() {
+        assertThat(array.get(1), is(2));
+    }
+}
