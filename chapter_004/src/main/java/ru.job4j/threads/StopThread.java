1c50ba9170c81fdca6f61457a8f4442358a17ff5
georgRusanov <rusanovgeorgy@gmail.com>
2018-03-25 00:22:17 +0300

    3. Реализовать механизм программнной остановки потока.

---
+++ chapter_004/src/main/java/ru.job4j/threads/StopThread.java
@@ -0,0 +1,59 @@
+package ru.job4j.threads;
+
+public class StopThread {
+    public static void main(String[] args) {
+    String string = "qwe a";
+
+    Thread thread = new Thread(new CountChar(string));
+    Thread time = new Thread(new Time(thread, 1));
+    time.start();
+    thread.start();
+    }
+}
+
+class  CountChar implements Runnable {
+    private String string;
+
+    public  CountChar(String string) {
+        this.string = string;
+    }
+    public void run() {
+        int result = 0;
+        try {
+            char[] chars = string.toCharArray();
+            int i = 0;
+            do {
+                if (!Thread.interrupted()) {
+                    if (chars[i] != ' ') {
+                        result++;
+                    }
+                    i++;
+                } else {throw new InterruptedException();}
+            } while (i < chars.length);
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            return;
+        }
+        System.out.println(result);
+    }
+}
+
+class Time implements Runnable {
+    private Thread thread;
+    private long limit;
+
+    Time(Thread thread, long limit) {
+        this.thread = thread;
+        this.limit = limit;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(limit);
+            thread.interrupt();
+        } catch (InterruptedException e) {
+            return;
+        }
+    }
+}
