2558b80633f0b3f71be22d6b76b391b61ee7b5cd
georgRusanov <rusanovgeorgy@gmail.com>
2018-03-24 22:13:45 +0300

    2. Сделать ожидание вывода.

--- chapter_004/src/main/java/ru.job4j/threads/Text.java
+++ chapter_004/src/main/java/ru.job4j/threads/Text.java
@@ -2,36 +2,55 @@ package ru.job4j.threads;
 
 public class Text {
 
-    public static final class Calculate implements Runnable {
-        private String string, search;
-
-        public Calculate(String string, String search) {
-            this.string = string;
-            this.search = search;
-        }
-        public void run() {
-            System.out.println(quantity(string, search));
-        }
-
-        int quantity(String string, String search) {
-            int gaps = 0;
-            int words = 0;
-            char[] chars = string.toCharArray();
-            if (chars[0] == ' ') {
-                gaps++;
-            } else { words++;}
-            for(int i = 1; i < chars.length; i++) {
-                if (chars[i] == ' ') {
-                    gaps++;
-                } else if (chars[i - 1] == ' ') { words++;}
-            }
-            return search.equals("gap") ? gaps : words;
-        }
-    }
-
     public static void main(String[] args) {
         String string = "qwe asd asd";
-        new Thread(new Calculate(string, "word")).start();
-        new Thread(new Calculate(string, "gap")).start();
+        Thread before = new Thread(() -> System.out.println("About"));
+        Thread tw = new Thread(new Calculate(string, "word"));
+        Thread tg = new Thread(new Calculate(string, "gap"));
+        Thread after = new Thread(() -> System.out.println("After all"));
+        before.start();
+        try {
+            before.join();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        tw.start();
+        tg.start();
+        try {
+            tw.join();
+            tg.join();
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        after.start();
+    }
+
+
+}
+
+class Calculate implements Runnable {
+    private String string, search;
+
+    public Calculate(String string, String search) {
+        this.string = string;
+        this.search = search;
+    }
+    public void run() {
+        System.out.println(quantity(string, search));
+    }
+
+    int quantity(String string, String search) {
+        int gaps = 0;
+        int words = 0;
+        char[] chars = string.toCharArray();
+        if (chars[0] == ' ') {
+            gaps++;
+        } else { words++;}
+        for(int i = 1; i < chars.length; i++) {
+            if (chars[i] == ' ') {
+                gaps++;
+            } else if (chars[i - 1] == ' ') { words++;}
+        }
+        return search.equals("gap") ? gaps : words;
     }
 }
