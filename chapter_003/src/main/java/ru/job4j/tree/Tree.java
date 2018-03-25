dc6af31ae438b042acc6886e46b7a9c51f0f9ecc
georgRusanov <rusanovgeorgy@gmail.com>
2018-03-25 12:29:45 +0300

    1. Создать элементарную структуру дерева

---
+++ chapter_003/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,76 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    Node<E> root;
+    Queue<Node<E>> dataIterator = new LinkedList<>();
+
+    public Tree(E value) {
+        this.root = new Node<>(value);
+        dataIterator.offer(this.root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = true;
+        if (findBy(parent).isPresent()) {
+            Node<E> parentNode = findBy(parent).get();
+            for (Node<E> element : parentNode.leaves()) {
+                if (element.eqValue(child)) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result) {
+                parentNode.add(new Node<>(child));
+            }
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            Optional<Node<E>> rsl = Optional.empty();
+
+            @Override
+            public boolean hasNext() {
+                return !dataIterator.isEmpty();
+            }
+
+            @Override
+            public Object next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("Больше нет элементов");
+                }
+                Node<E> el = dataIterator.poll();
+                rsl = Optional.of(el);
+                for (Node<E> child : el.leaves()) {
+                    dataIterator.offer(child);
+                }
+                return rsl.get().getValue();
+            }
+        };
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class TreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void invocationOfNextMethodShouldThrowNoSuchElementException() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(3, 5);
+        assertThat(tree.iterator().next(), is(1));
+        assertThat(tree.iterator().next(), is(2));
+        assertThat(tree.iterator().next(), is(3));
+        assertThat(tree.iterator().next(), is(4));
+        assertThat(tree.iterator().next(), is(5));
+        tree.iterator().next();
+    }
+}
