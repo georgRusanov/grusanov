62d84a0b16285d27fb0b6774de091ec12485c26b
georgRusanov <rusanovgeorgy@gmail.com>
2018-04-01 00:01:45 +0300

    2. Реализовать ThreadPool

---
+++ chapter_004/src/main/java/ru.job4j/wait/ThreadsPool.java
@@ -0,0 +1,49 @@
+package ru.job4j.wait;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class ThreadsPool {
+    @GuardedBy("this")
+    private Queue<Work> queue = new LinkedList<>();
+    private boolean condition = true;
+
+    public ThreadsPool() {
+        for (int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+            new Thread(new workThread()).start();
+        }
+    }
+
+    void add(Work work) {
+        queue.add(work);
+        synchronized (queue) {
+            queue.notify();
+        }
+    }
+
+    private final class workThread implements Runnable {
+
+        @Override
+        public void run() {
+            while (condition) {
+                synchronized (queue) {
+                    while (queue.isEmpty()) {
+                        try {
+                            queue.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    queue.poll();
+                }
+            }
+        }
+    }
+
+    class Work {}
+}
+
