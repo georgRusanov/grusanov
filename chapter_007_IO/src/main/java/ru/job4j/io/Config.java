c127ee16c9f52808656a7ac98ff1ed3921db5ed3
georgRusanov <rusanovgeorgy@gmail.com>
2019-08-18 15:36:57 +0300

    task 858

---
+++ chapter_007_IO/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines()
+                .filter(line -> !line.startsWith("#") && !line.isEmpty())
+                    .peek(line -> {
+                        if(line.chars()
+                                .filter(ch -> ch == '=')
+                                .count() != 1)
+                            throw new IllegalArgumentException("Wrong property format. Use one char '=' in string.");
+                    })
+                    .map(line -> line.split("="))
+                .forEach(entry -> values.put(entry[0], entry[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ chapter_007_IO/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+
+public class ConfigTest {
+    private File file;
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Before
+    public void createTempFolder() throws IOException {
+        file = temporaryFolder.newFile("configTestFile.txt");
+    }
+
+    @Test
+    public void positiveTest() {
+        saveToFile("#a=b\n\r\n\rc=d");
+        Config config = new Config(file.getPath());
+        config.load();
+        assertThat(config.value("c"), is("d"));
+        assertThat(config.value("a"), isEmptyOrNullString());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void negativeTest() {
+        saveToFile("#a=b\n\r\n\rc=d=");
+        Config config = new Config(file.getPath());
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExpected()
+    {
+        throw new IllegalArgumentException();
+    }
+
+    private void saveToFile(String text) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
