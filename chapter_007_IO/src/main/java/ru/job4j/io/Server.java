4e17c0d5821c652349fb46ad44bef666e9bce760
georg <rusanovgeorgy@gmail.com>
2020-02-05 23:13:32 +0300

    added Server with tests

---
+++ chapter_007_IO/src/main/java/ru/job4j/io/Server.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private final Socket socket;
+
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() throws IOException {
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String ask = null;
+        do {
+            System.out.println("wait command ...");
+            ask = in.readLine();
+            System.out.println(ask);
+            if ("hello".equalsIgnoreCase(ask)) {
+                out.println("Hello, dear friend, I'm a oracle.");
+                out.println();
+            } else if (!"exit".equalsIgnoreCase(ask)) {
+                out.println("I don't understand");
+            }
+        } while (!"exit".equalsIgnoreCase(ask));
+    }
+
+    public static void main(String[] args) throws IOException{
+        try(final Socket socket = new ServerSocket(2000).accept()) {
+            new Server(socket);
+        }
+    }
+}
---
+++ chapter_007_IO/src/test/java/ru/job4j/io/ServerTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.io;
+
+import com.google.common.base.Joiner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
+public class ServerTest {
+    private static final String NL = System.getProperty("line.separator");
+    private String input;
+    private String output;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {"exit", ""},
+                {Joiner.on(NL).join("hello", "exit"), String.format("Hello, dear friend, I'm a oracle.%s%s", NL, NL)},
+                {Joiner.on(NL).join("hi", "exit"), String.format("I don't understand%s", NL)}
+        });
+    }
+
+    public ServerTest(String input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Test
+    public void serverTest() throws IOException {
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Socket socket = mock(Socket.class);
+        when(socket.getInputStream()).thenReturn(in);
+        when(socket.getOutputStream()).thenReturn(out);
+        Server server = new Server(socket);
+        server.run();
+        assertThat(out.toString(), is(output));
+    }
+}
